a8a908a76ab37c467fc29855badd72cfd07b8867
juliashalneva <yuliashalneva@yandex.ru>
2018-10-07 19:44:48 +0500

    Первое задание

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string A = Console.ReadLine();
+            int lenght = A.Length;
+            //Console.WriteLine(lenght);
+            int N = int.Parse(A);
+            int M = 0;
+            while (N>0)
+            {
+                M += (N % 10) * (int)Math.Pow( 10, lenght - 1);
+                lenght--;
+                N /= 10;
+            }
+            Console.WriteLine(M);
+        }
+    }
+}
